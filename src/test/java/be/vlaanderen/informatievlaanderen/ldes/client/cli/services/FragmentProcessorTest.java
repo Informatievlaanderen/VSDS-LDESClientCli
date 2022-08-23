fbcb8e9599fe323f51efceccb00862e6b78fce58
WLefever-Cegeka <104509963+WLefever-Cegeka@users.noreply.github.com>
2022-08-23 10:35:50 +0200

    feature: extract CLI (#2)
    * feature: extract CLI
    
    * PR remarks

---
+++ src/test/java/be/vlaanderen/informatievlaanderen/ldes/client/cli/services/FragmentProcessorTest.java
@@ -0,0 +1,77 @@
+package be.vlaanderen.informatievlaanderen.ldes.client.cli.services;
+
+import be.vlaanderen.informatievlaanderen.ldes.client.services.LdesService;
+import be.vlaanderen.informatievlaanderen.ldes.client.valueobjects.LdesFragment;
+import be.vlaanderen.informatievlaanderen.ldes.client.valueobjects.LdesMember;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFParserBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class FragmentProcessorTest {
+    LdesService ldesService = mock(LdesService.class);
+
+    @Test
+    void when_LdesServerHasFragments_TheyAreConvertedAndPrintedOut() throws IOException, URISyntaxException {
+        when(ldesService.hasFragmentsToProcess()).thenReturn(true);
+        LdesFragment ldesFragment = new LdesFragment();
+        LdesMember ldesMember = readLdesMemberFromFile(getClass().getClassLoader(), "member1.txt");
+        ldesFragment.addMember(ldesMember);
+        when(ldesService.processNextFragment()).thenReturn(ldesFragment);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        FragmentProcessor fragmentProcessor = new FragmentProcessor(ldesService, printStream, Lang.NQUADS);
+        fragmentProcessor.processLdesFragments();
+
+        byteArrayOutputStream.flush();
+        String actualOutput = byteArrayOutputStream.toString();
+        assertTrue(ldesMember.getMemberModel().isIsomorphicWith(convertToModel(actualOutput)));
+    }
+
+    @Test
+    void when_LdesServerHasNoFragments_NothingIsPrinted() throws IOException {
+        when(ldesService.hasFragmentsToProcess()).thenReturn(false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        FragmentProcessor fragmentProcessor = new FragmentProcessor(ldesService, printStream, null);
+        fragmentProcessor.processLdesFragments();
+
+        byteArrayOutputStream.flush();
+        String actualOutput = byteArrayOutputStream.toString();
+        assertEquals("", actualOutput);
+    }
+
+    private LdesMember readLdesMemberFromFile(ClassLoader classLoader, String fileName)
+            throws URISyntaxException, IOException {
+        File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).toURI());
+        String memberString = Files.lines(Paths.get(file.toURI())).collect(Collectors.joining());
+        Model outputModel = convertToModel(memberString);
+
+        return new LdesMember("https://private-api.gipod.beta-vlaanderen.be/api/v1/mobility-hindrances/10810464/1",
+                outputModel);
+    }
+
+    private Model convertToModel(String memberString) {
+        return RDFParserBuilder.create()
+                .fromString(memberString).lang(Lang.NQUADS)
+                .toModel();
+    }
+
+}
