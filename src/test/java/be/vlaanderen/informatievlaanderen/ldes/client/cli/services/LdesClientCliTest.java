04533e4b4e2887c184b6062dd8315b30aee73408
WLefever <wouter.lefever@cegeka.com>
2022-09-19 11:13:41 +0200

    Feature: VSDSPUB-287: Increase coverage services

--- src/test/java/be/vlaanderen/informatievlaanderen/ldes/client/cli/services/LdesClientCliTest.java
+++ src/test/java/be/vlaanderen/informatievlaanderen/ldes/client/cli/services/LdesClientCliTest.java
@@ -2,9 +2,14 @@ package be.vlaanderen.informatievlaanderen.ldes.client.cli.services;
 
 import be.vlaanderen.informatievlaanderen.ldes.client.cli.model.EndpointBehaviour;
 import org.apache.jena.riot.Lang;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.*;
 
@@ -13,12 +18,22 @@ class LdesClientCliTest {
 	ExecutorService executorService = mock(ExecutorService.class);
 	LdesClientCli ldesClientCli = new LdesClientCli(executorService);
 
-	@Test
-	void when_LdesClientCliIsStarted_TaskOfClassCliRunnerIsSubmitted() {
-		ldesClientCli.start("fragmentId", Lang.NQUADS, Lang.TURTLE, 1000L, 20L, EndpointBehaviour.STOPPING);
+	@ParameterizedTest
+	@ArgumentsSource(EndpointBehavoirArgumentsProvider.class)
+	void when_LdesClientCliIsStarted_TaskOfClassCliRunnerIsSubmitted(final EndpointBehaviour endpointBehaviour) {
+		ldesClientCli.start("fragmentId", Lang.NQUADS, Lang.TURTLE, 1000L, 20L, endpointBehaviour);
 
 		verify(executorService, times(1)).submit(any(CliRunner.class));
 		verify(executorService, times(1)).shutdown();
+	}
+
+	static class EndpointBehavoirArgumentsProvider implements ArgumentsProvider {
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+			return Stream.of(
+					Arguments.of(EndpointBehaviour.STOPPING),
+					Arguments.of(EndpointBehaviour.WAITING));
+		}
 	}
 
 }
